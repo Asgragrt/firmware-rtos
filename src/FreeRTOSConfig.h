847283b6d9ee8038e87e96dc5887fee45695dda5
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-02-08 18:08:05 -0600

    Disabled time slicing

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -46,6 +46,7 @@
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_MINIMAL_IDLE_HOOK             0
 #define configUSE_TICK_HOOK                     0
+#define configUSE_PASSIVE_IDLE_HOOK             0
 #define configTICK_RATE_HZ                      ( ( TickType_t ) 20000 )
 #define configMAX_PRIORITIES                    32
 #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 256
@@ -60,7 +61,7 @@
 #define configUSE_COUNTING_SEMAPHORES           1
 #define configQUEUE_REGISTRY_SIZE               8
 #define configUSE_QUEUE_SETS                    1
-#define configUSE_TIME_SLICING                  1
+#define configUSE_TIME_SLICING                  0
 #define configUSE_NEWLIB_REENTRANT              0
 // todo need this for lwip FreeRTOS sys_arch to compile
 #define configENABLE_BACKWARD_COMPATIBILITY     1
