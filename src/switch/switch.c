f9479934cf963cf99063da17c7caf3d03a9285c3
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-03-03 20:49:38 -0600

    Assymetric debounce

--- src/switch/switch.c
+++ src/switch/switch.c
@@ -4,6 +4,7 @@ switch_t new_switch(uint8_t pin){
     switch_t sw = {
         .pin = pin,
         .debounce = 0,
+        .wait = 0,
         .status = SW_NOT_PRESSED,
     };
     return sw;
@@ -13,27 +14,17 @@ bool sw_is_pressed( switch_t* sw ){
     return !gpio_get( sw->pin );
 }
 
-void sw_debounce(switch_t* sw){
-    // Here goes the debounce logic
-    /*
-    if (sw_is_pressed(sw)){
-        sw->debounce = SW_DEBOUCE_TIME;
-        sw->status = SW_PRESSED;
-        return;
-    } else if (sw->debounce) {
-        sw->debounce >>= 1;
-    } else {
-        sw->status = SW_NOT_PRESSED;
-    }
-    */
-    
+void sw_debounce(switch_t* sw){    
     sw->debounce <<= 1;
     sw->debounce |= sw_is_pressed(sw);
+    sw->wait >>= 1;
 }
 
 SW_STATUS sw_get_status( switch_t* sw ){
-    
     if ( __builtin_popcount( (uint32_t) (sw->debounce) ) >= 7 ) {
+        sw->wait = SW_DEBOUCE_TIME;
+    }
+    if ( sw->wait ) {
         sw->status = SW_PRESSED;
     } else {
         sw->status = SW_NOT_PRESSED;
--- src/switch/switch.h
+++ src/switch/switch.h
@@ -1,11 +1,11 @@
 #ifndef _SWITCH_H_
 #define _SWITCH_H_
 
-#include "pico/stdlib.h"
 #include "FreeRTOS.h"
+#include "pico/stdlib.h"
 
-#define DEBOUCE_SAMPLE_TICKS pdMS_TO_TICKS(1) / 4
-#define SW_DEBOUCE_TIME 0b11111
+#define DEBOUCE_SAMPLE_TICKS pdMS_TO_TICKS( 1 ) / 4
+#define SW_DEBOUCE_TIME      0xFFFFF;
 
 typedef enum {
     SW_NOT_PRESSED,
@@ -15,12 +15,13 @@ typedef enum {
 typedef struct {
     uint8_t pin;
     uint8_t debounce;
+    uint32_t wait;
     SW_STATUS status;
 } switch_t;
 
-switch_t new_switch(uint8_t pin);
+switch_t new_switch( uint8_t pin );
 
-void sw_debounce(switch_t* sw);
+void sw_debounce( switch_t* sw );
 
 SW_STATUS sw_get_status( switch_t* sw );
 
